3295c947804af7bc32601c8db571811d9b1a6440
Hassan El Ouardy <hassan.elouardy06@gmail.com>
2025-09-15 12:39:38 +0100

    npm uninstall lucide-react

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import { Menu, User } from "lucide-react";
 import "./App.css";
 
 function App() {
 	return (
 		<div className="text-center mt-8 font-bold text-orange-500">
+			<div className="flex justify-between p-4 border border-gray-300 rounded-lg ">
+				<Menu />
+				<User />
+			</div>
 			App component
 		</div>
 	);

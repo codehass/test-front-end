a61ed94c1598343f127005e6eefb14386a3dec15
Hassan El Ouardy <hassan.elouardy06@gmail.com>
2025-09-15 17:07:52 +0100

    create TotalIssuesCard component

---
+++ src/components/TotalIssuesCard.tsx
@@ -0,0 +1,11 @@
+const TotalIssuesCard = () => (
+	<div className="row-span-2 bg-white flex flex-col pt-5 items-center gap-4 rounded-3xl">
+		<p className="font-medium">Total Issues</p>
+		<div className="bg-[#77cfbf] w-20 h-20 rounded-full flex items-center justify-center">
+			<div className="text-3xl font-bold bg-white w-16 h-16 rounded-full flex items-center justify-center">
+				22
+			</div>
+		</div>
+	</div>
+);
+export default TotalIssuesCard;
